9bc13eb02305e37ddabe5ebc6460b7ede7a779f5
edson80 <soerbhakty@gmail.com>
2017-10-11 00:02:33 +0700

    Tugas Geeks Farm Training

--- eventlistener/app/src/main/java/com/geeksfarm/eventlistener/Viewdata.java
+++ eventlistener/app/src/main/java/com/geeksfarm/eventlistener/Viewdata.java
@@ -11,25 +11,25 @@ import android.widget.TextView;
 
 public class Viewdata extends AppCompatActivity {
 
-    TextView nama,alamat,tgllahir;
-    String get_nama,get_alamat,get_tgllahir;
+    TextView nama,alamat,email;
+    String get_nama,get_alamat,get_email;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.show_data);
 
         nama = (TextView) findViewById(R.id.nama_textView);
         alamat = (TextView) findViewById(R.id.phone_textView);
-        tgllahir = (TextView) findViewById(R.id.email_textView);
+        email = (TextView) findViewById(R.id.email_textView);
 
         Bundle b = getIntent().getExtras();
 
         get_nama = b.getString("parse_nama");
         get_alamat = b.getString("parse_phone");
-        get_tgllahir = b.getString("parse_mail");
+        get_email = b.getString("parse_mail");
 
         nama.setText("Nama : "+get_nama);
         alamat.setText("Alamat : "+get_alamat);
-        tgllahir.setText("Tempat/Tanggal Lahir : "+get_tgllahir);
+        email.setText("email : "+get_email);
 
     }
 }
